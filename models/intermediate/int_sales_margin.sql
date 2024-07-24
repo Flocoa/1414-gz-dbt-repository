233f569dfb81a4f5a9f69c5c0107fc0695b48470
Florian Coadic <florian@coadic.fr>
2024-07-24 09:58:01 +0000

    update staging

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,8 @@ SELECT
     quantity, 
     purchase_price, 
     ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*p.purchase_price,2) AS margin
+    s.revenue - ROUND(s.quantity*p.purchase_price,2) AS margin,
+    {{ margin_percent('s.revenue', 's.quantity*CAST(p.purchSE_PRICE AS FLOAT64)') }} AS margin_macro
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
 	USING (products_id)
